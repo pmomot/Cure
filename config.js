ea116f2ec156832a605eb573ee8aff8d8a849cff
ganzerga <vyokhna@malkosua.com>
2016-01-14 18:33:58 +0200

    favicon changed

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ module.exports = {
     //"database": "mongodb://root:rootRat@ds029297.mongolab.com:29297/malkos-internal-needs",
     "database": "mongodb://localhost/",
     "port": 3000,
-    //"port": 4000,
     //"port": process.env || 3000,
     "secretKey": "SomeVerySecretStuff111"
 };
